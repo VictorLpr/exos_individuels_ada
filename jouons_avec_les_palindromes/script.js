c1d4b7c7bc60ba029cb9b6e6a2ecae87d7bbcdc3
VictorLpr <victorlepron@gmail.com>
2025-03-11 10:59:47 +0100

    palidrome etape 4

--- jouons_avec_les_palindromes/script.js
+++ jouons_avec_les_palindromes/script.js
@@ -1,7 +1,7 @@
 const millInDay = 1000 * 60 * 60 * 24
 let date = new Date
 
-function isValidDate (date) {
+function isValidDate(date) {
     let [day, month, year] = date.split("/");
     if (!(parseInt(day)) || day.length != 2 || !(parseInt(month)) || month.length != 2 || !(parseInt(year)) || year.length != 4) {
         return false
@@ -9,13 +9,13 @@ function isValidDate (date) {
         return false
     } else if (day > maxDaysInMonth(month)) {
         return false
-    } else if (month == '02' && day == '29' && year%4 != 0) {
+    } else if (month == '02' && day == '29' && year % 4 != 0) {
         return false
     }
     return true
 }
 
-function maxDaysInMonth (month) {
+function maxDaysInMonth(month) {
     switch (month) {
         case '01':
         case '03':
@@ -34,29 +34,26 @@ function maxDaysInMonth (month) {
             break;
         case '02':
             return 29;
-            break; 
+            break;
     }
 }
 
 
-function isPalindrome (date) {
+function isPalindrome(string) {
     let res = true;
-    if (!(isValidDate(date))){
-        res = false;
-    } else {
-        let string = date.split("/").join("");
-        for (let i = 0; i < string.length; i++) {
-            if (string[i] != string[string.length - 1 - i]) {
-                res = false;
-            }
+
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] != string[string.length - 1 - i]) {
+            res = false;
         }
     }
+
     return res;
 }
 
-function getNextPalindromes (x) {
-    while (x > 0){
-        if (isPalindrome(date.toLocaleDateString())) {
+function getNextPalindromes(x) {
+    while (x > 0) {
+        if (isDatePalindrome(date.toLocaleDateString())) {
             console.log(date.toLocaleDateString());
             x--;
         }
@@ -66,6 +63,19 @@ function getNextPalindromes (x) {
     }
 }
 
-// console.log(isValidDate("22/12/2222"))
+function isDatePalindrome(date) {
+    if (!(isValidDate(date))) {
+        return false
+    } else {
+        let string = date.split("/").join("");
+        return (isPalindrome(string))
+    }
+}
+
+
+console.log(isDatePalindrome("22/12/2222"))
+console.log(isDatePalindrome("30/02/2003"))
+console.log(isDatePalindrome("22/12/2122"))
+
 // console.log(isPalindrome(date.toLocaleDateString()))
-getNextPalindromes(2)
+getNextPalindromes(10)
