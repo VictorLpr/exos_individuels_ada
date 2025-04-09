5394e03a37947a2280726558d8d56aacde8b5815
VictorLpr <victorlepron@gmail.com>
2025-04-09 11:54:16 +0200

    etape 3

---
+++ motus/script.js
@@ -0,0 +1,41 @@
+function tryWord(word, base) {
+    // TODO: fix jeu sensible à la casse.
+    if (word === base) {
+        return true
+    } else {
+        let arrayBase = base.split('');
+        let arrayWord = word.split('');
+        let wellPlaced = [];
+        let notInWord = [];
+        let missplaced = [];
+
+
+        for (let i = 0; i < arrayWord.length; i++) {
+            if (arrayBase[i] === arrayWord[i]) {
+                wellPlaced.push(arrayWord[i]);
+            } else if (arrayBase.includes(arrayWord[i])) {
+                missplaced.push(arrayWord[i])
+            } else {
+                notInWord.push(arrayWord[i])
+            }
+        }
+        return { wellPlaced: wellPlaced, missplaced: missplaced, notInWord: notInWord }
+    }
+}
+
+function guess() {
+    let base = 'dictionnaire'
+    let wordInput = document.getElementById("word")
+    let word = wordInput.value.toLowerCase()
+    let result = tryWord(word, base)
+
+    console.log(result)
+    if (result === true) {
+        document.getElementById("win").innerText = 'Vous avez gagné'
+    }
+    wordInput.value = ''
+    document.getElementById("try").innerText = word
+    document.getElementById("well").innerText = result.wellPlaced ? `Bien placé: ${result.wellPlaced.join(', ')}` : ""
+    document.getElementById("miss").innerText = result.missplaced ? `Mal placé: ${result.missplaced.join(', ')}` : ""
+    document.getElementById("not").innerText = result.notInWord ? `Pas dans le mot: ${result.notInWord.join(', ')}` : ""
+}
