ff1d319091596505e520b7a91259843d73a16190
VictorLpr <victorlepron@gmail.com>
2025-06-25 15:29:27 +0200

    Poker etape 3

--- poker/main.ts
+++ poker/main.ts
@@ -38,8 +38,24 @@ function deal(count: number, deck: string[]): string[] {
   return deck.splice(0, count);
 }
 
+function flop(deck: string[]): string[] {
+  const cards: string[] = [];
+
+  deal(1, deck);
+  cards.push(...deal(3, deck));
+
+  deal(1, deck);
+  cards.push(...deal(1, deck));
+
+  deal(1, deck);
+  cards.push(...deal(1, deck));
+
+  return cards;
+}
+
 const newDeck = createDeck();
 const player1 = deal(2, newDeck);
 const player2 = deal(2, newDeck);
-
+const cards = flop(newDeck);
 console.log(player1);
+console.log(cards);
